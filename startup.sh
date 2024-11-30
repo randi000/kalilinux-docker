c4d0b55af404003161ab5e530cf060bc692ca4a3
randi000 <d.emon.waifu414@gmail.com>
2024-11-30 17:01:50 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,4 +10,4 @@ rm -rf ~/.vnc/*.pid ~/.vnc/*.log /tmp/.X1*
 vncpasswd -f <<< ${PASSWORD} > ~/.vnc/passwd
 vncserver -PasswordFile ~/.vnc/passwd
 sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
-/usr/share/novnc/utils/novnc_proxy --listen 8080 --vnc 127.0.0.1:5901
+/usr/share/novnc/utils/novnc_proxy --listen 3000 --vnc 127.0.0.1:5901
